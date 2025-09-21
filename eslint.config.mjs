137d0bcad3fba427eb2f54ead26095ba60c533ef
Derzsi Dezső <derzsidezso@gmail.com>
2025-09-21 19:00:35 +0300

    chore: add ESLint and Husky configurations, remove Tailwind CSS setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,4 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt({
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:unicorn/recommended',
-    'plugin:import/recommended',
-    'plugin:security/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['vue', '@typescript-eslint', 'unicorn', 'import', 'security', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    // Add custom rules here if needed
-  },
-});
+export default withNuxt();
+// Add any custom ESLint config here
